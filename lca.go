982186084b7fc1aa5a7d8e0297717bbdc10aeb7b
Dermot <contact@dermotobrien.me>
2021-10-09 20:06:57 +0100

    Define graph content and fill

--- lca.go
+++ lca.go
@@ -8,4 +8,24 @@ type Node struct {
 }
 
 func main() {
+	/**
+	*		 john
+	*	    /    \
+	*	 pat      mary
+	*   /  \      /   \
+	* mat sarah  mike  duke
+	*
+	 */
+	x := Node{"john", nil, nil}
+	x.left = &Node{"pat", nil, nil}
+	x.right = &Node{"mary", nil, nil}
+
+	x.left.left = &Node{"mat", nil, nil}
+	x.left.right = &Node{"sarah", nil, nil}
+
+	x.right.left = &Node{"mike", nil, nil}
+	x.right.right = &Node{"duke", nil, nil}
+
+	fmt.Print(x)
+
 }
