271baabfd3d356de693f394287cdb6b2d046e9bb
Dermot <contact@dermotobrien.me>
2021-10-09 21:47:44 +0100

    TestMissingReference Test

--- lca_test.go
+++ lca_test.go
@@ -34,3 +34,24 @@ func TestLCA(t *testing.T) {
 	fmt.Print(lcaResp.data)
 
 }
+
+func TestMissingReference(t *testing.T) {
+	/**
+	*		 	 john
+	*	    	/    \
+	*	 	pat      mary
+	*
+	 */
+
+	x := Node{"john", nil, nil}
+	x.left = &Node{"pat", nil, nil}
+	x.right = &Node{"mary", nil, nil}
+
+	lcaResp := lca(&x, "pat", "doesNotExistOnTree")
+
+	if lcaResp != nil {
+		t.Errorf(`lca(Node, "pat", "mike") = %s; wants nil`, lcaResp.data)
+	}
+}
+
+// TODO: Write more tests
