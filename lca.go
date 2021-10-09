4bab2611e8e1a4779889eacbde9ef40a7900bb82
Dermot <contact@dermotobrien.me>
2021-10-09 21:08:15 +0100

    Function: Check if data point exists in tree

--- lca.go
+++ lca.go
@@ -8,14 +8,16 @@ type Node struct {
 }
 
 func main() {
+
 	/**
-	*		 john
-	*	    /    \
-	*	 pat      mary
-	*   /  \      /   \
-	* mat sarah  mike  duke
+	*		 	 john
+	*	    	/    \
+	*	 	pat      mary
+	*   	/  \      /   \
+	*	 mat sarah  mike  duke
 	*
 	 */
+
 	x := Node{"john", nil, nil}
 	x.left = &Node{"pat", nil, nil}
 	x.right = &Node{"mary", nil, nil}
@@ -27,5 +29,19 @@ func main() {
 	x.right.right = &Node{"duke", nil, nil}
 
 	fmt.Print(x)
+func pointExistsOnTree(currentNode *Node, data string) bool {
+
+	/** Ensure root and data intialised */
+	if currentNode == nil || len(data) == 0 {
+		return false
+	}
+
+	/** Check if data is the currentNode */
+	if currentNode.data == data {
+		return true
+	}
+
+	/** Loop recursivly through left and right nodes to check if data exists */
+	return pointExistsOnTree(currentNode.left, data) || pointExistsOnTree(currentNode.right, data)
 
 }
