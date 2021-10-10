87669622819bfac552cac0842c77555c532ff529
Dermot <contact@dermotobrien.me>
2021-10-10 13:52:08 +0100

    Test large tree for multiple LCA

--- Go/lca_test.go
+++ Go/lca_test.go
@@ -43,3 +43,54 @@ func TestMissingReference(t *testing.T) {
 	}
 }
 
+func TestLargeTree(t *testing.T) {
+	/**
+	* Initialise tree structure with data
+	*
+	*		 	 john
+	*	    	/    \
+	*	 	pat      mary
+	*   	/  \      /   \
+	*	 mat sarah  mike  duke
+	*
+	 */
+
+	x := Node{"john", nil, nil}
+	x.left = &Node{"pat", nil, nil}
+	x.right = &Node{"mary", nil, nil}
+
+	x.left.left = &Node{"mat", nil, nil}
+	x.left.right = &Node{"sarah", nil, nil}
+
+	x.right.left = &Node{"mike", nil, nil}
+	x.right.right = &Node{"duke", nil, nil}
+
+	/** Initialise answer table for testing */
+	lcaAnswerTable := []struct {
+		predecessor1   string
+		predecessor2   string
+		commonAncestor string
+	}{
+		{"pat", "mary", "john"},
+		{"mat", "sarah", "pat"},
+		{"mike", "duke", "mary"},
+		{"mat", "pat", "pat"},
+		{"mary", "sarah", "john"},
+	}
+
+	/** Test LCA algorithm against answer table */
+	for _, lcaAnswer := range lcaAnswerTable {
+		lcaResp := lca(&x, lcaAnswer.predecessor1, lcaAnswer.predecessor2)
+
+		if lcaResp.data != lcaAnswer.commonAncestor {
+			t.Errorf(`lca(Node, "%s", "%s") = %s; wants "%s"`,
+				lcaAnswer.predecessor1,
+				lcaAnswer.predecessor2,
+				lcaResp.data,
+				lcaAnswer.commonAncestor,
+			)
+
+		}
+	}
+}
+
