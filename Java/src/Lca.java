ff649d8180f810c494d3dafa51f03438740919f4
Dermot <contact@dermotobrien.me>
2021-10-10 14:48:54 +0100

    Create Lca.java

---
+++ Java/src/Lca.java
@@ -0,0 +1,11 @@
+package Java.src;
+public class Lca {
+    public Node head;
+    public String a, b;
+
+    public Lca(Node head, String a, String b) {
+        this.head = head;
+        this.a = a;
+        this.b = b;
+    }
+}
