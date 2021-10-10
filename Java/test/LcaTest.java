0617db84f80ff17da84448b1f575ba5a61ad09d0
Dermot <contact@dermotobrien.me>
2021-10-10 14:48:19 +0100

    Initialise LcaTest

--- Java/test/LcaTest.java
+++ Java/test/LcaTest.java
@@ -1,5 +1,13 @@
 package Java.test;
+
+import Java.src.Lca;
+import Java.src.Node;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
+
 import static org.junit.Assert.*;
+
+@RunWith(JUnit4.class)
+public class LcaTest {
+}
